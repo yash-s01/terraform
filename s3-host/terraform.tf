7a6a73973ab51df4796f5a24fa35a6d5befd2b96
yashwant <yashwantbellii@gmail.com>
2024-02-13 19:21:44 +0530

    modified s3-host terraform file

--- s3-host/terraform.tf
+++ s3-host/terraform.tf
@@ -4,6 +4,8 @@ resource "aws_s3_bucket" "web-1234" {
   force_destroy = true
 }
 
+###### configure website pages#######
+
 resource "aws_s3_bucket_website_configuration" "website" {
   bucket = aws_s3_bucket.web-1234.id
   index_document {
@@ -14,6 +16,8 @@ resource "aws_s3_bucket_website_configuration" "website" {
   }
 }
 
+###### add policy for public access #######
+
 resource "aws_s3_bucket_public_access_block" "public_access_block" {
   bucket = aws_s3_bucket.web-1234.id
   block_public_acls       = false
@@ -23,6 +27,7 @@ resource "aws_s3_bucket_public_access_block" "public_access_block" {
 }
 
 ##### will upload all the files present under HTML folder to the S3 bucket #####
+
 resource "aws_s3_object" "upload_object" {
   for_each      = fileset("/Users/yashwant/Downloads/build_3/", "**/*")
   bucket        = aws_s3_bucket.web-1234.id
@@ -31,9 +36,15 @@ resource "aws_s3_object" "upload_object" {
   etag          = filemd5("/Users/yashwant/Downloads/build_3/${each.value}")
   content_type  = "text/html"
 }
+
+#### for obtaining the endpoint####
+
 output "s3_bucket_id" {
   value = aws_s3_bucket_website_configuration.website.website_endpoint
 }
+
+#####update bucket policy for get object###########
+
 resource "aws_s3_bucket_policy" "web-1234" {
   bucket = aws_s3_bucket.web-1234.id
 
