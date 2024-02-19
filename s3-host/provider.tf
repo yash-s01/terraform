5d793d3c0635062a61666386048d729ddd5e7368
yashwant <yashwantbellii@gmail.com>
2024-02-19 18:28:16 +0530

    changes in bucket policy style

---
+++ s3-host/provider.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+  region = var.region
+  access_key = var.a_key
+  secret_key = var.sec_key
+}
--- s3-host/terraform.tf
+++ s3-host/terraform.tf
@@ -1,7 +1,31 @@
 ##### Creating an S3 Bucket #####
+
 resource "aws_s3_bucket" "web-1234" {
   bucket = "tf-web-1234-buck"
   force_destroy = true
+  tags = {
+   "Owner" = "root"
+  }
+}
+
+resource "aws_s3_bucket_policy" "name" {
+  bucket = aws_s3_bucket.web-1234.id
+  policy = data.aws_iam_policy_document.website_policy.json
+}
+
+data "aws_iam_policy_document" "website_policy" {
+  statement {
+    actions = [
+      "s3:GetObject"
+    ]
+    principals {
+      identifiers = ["*"]
+      type = "AWS"
+    }
+    resources = [
+      "${aws_s3_bucket.web-1234.arn}/*"
+    ]
+  }
 }
 
 ###### configure website pages#######
@@ -16,6 +40,25 @@ resource "aws_s3_bucket_website_configuration" "website" {
   }
 }
 
+# #####update bucket policy for get object###########
+
+# resource "aws_s3_bucket_policy" "web-1234" {
+#   bucket = aws_s3_bucket.web-1234.id
+
+#   policy = jsonencode({
+#     Version = "2012-10-17",
+#     Statement = [
+#       {
+#         Sid       = "PublicReadGetObject",
+#         Effect    = "Allow",
+#         Principal = "*",
+#         Action    = "s3:GetObject",
+#         Resource  = "${aws_s3_bucket.web-1234.arn}/*"
+#       }
+#     ]
+#   })
+# }
+
 ###### add policy for public access #######
 
 resource "aws_s3_bucket_public_access_block" "public_access_block" {
@@ -26,15 +69,14 @@ resource "aws_s3_bucket_public_access_block" "public_access_block" {
   restrict_public_buckets = false
 }
 
-##### will upload all the files present under HTML folder to the S3 bucket #####
+  ##### will upload all the files present under build_3 folder to the S3 bucket #####
 
 resource "aws_s3_object" "upload_object" {
-  for_each      = fileset("/Users/yashwant/Downloads/build_3/", "**/*")
+  for_each      = fileset("/Users/yashwant/Downloads/build_3", "**/*")
   bucket        = aws_s3_bucket.web-1234.id
   key           = each.value
   source        = "/Users/yashwant/Downloads/build_3/${each.value}"
-  etag          = filemd5("/Users/yashwant/Downloads/build_3/${each.value}")
-  content_type  = "text/html"
+  content_type = "text/html"
 }
 
 #### for obtaining the endpoint####
@@ -43,21 +85,4 @@ output "s3_bucket_id" {
   value = aws_s3_bucket_website_configuration.website.website_endpoint
 }
 
-#####update bucket policy for get object###########
 
-resource "aws_s3_bucket_policy" "web-1234" {
-  bucket = aws_s3_bucket.web-1234.id
-
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Sid       = "PublicReadGetObject",
-        Effect    = "Allow",
-        Principal = "*",
-        Action    = "s3:GetObject",
-        Resource  = "${aws_s3_bucket.web-1234.arn}/*"
-      }
-    ]
-  })
-}
