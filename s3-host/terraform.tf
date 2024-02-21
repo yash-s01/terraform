a7780c65d37a110a4dd14bdd07a349f3114285e2
yashwant <yashwantbellii@gmail.com>
2024-02-21 11:36:42 +0530

    fixed policy issues and build upload issues

--- s3-host/terraform.tf
+++ s3-host/terraform.tf
@@ -16,7 +16,7 @@ resource "aws_s3_bucket_policy" "name" {
 data "aws_iam_policy_document" "website_policy" {
   statement {
     actions = [
-      "s3:GetObject"
+      "s3:*"
     ]
     principals {
       identifiers = ["*"]
@@ -69,14 +69,24 @@ resource "aws_s3_bucket_public_access_block" "public_access_block" {
   restrict_public_buckets = false
 }
 
-  ##### will upload all the files present under build_3 folder to the S3 bucket #####
+##### will upload all the files present under build_3 folder to the S3 bucket #####
 
-resource "aws_s3_object" "upload_object" {
-  for_each      = fileset("/Users/yashwant/Downloads/build_3", "**/*")
-  bucket        = aws_s3_bucket.web-1234.id
-  key           = each.value
-  source        = "/Users/yashwant/Downloads/build_3/${each.value}"
-  content_type = "text/html"
+# resource "aws_s3_object" "upload_object" {
+#   for_each      = fileset("/Users/yashwant/Downloads/build_3", "**/*")
+#   bucket        = aws_s3_bucket.web-1234.id
+#   key           = each.value
+#   source        = "/Users/yashwant/Downloads/build_3/${each.value}"
+#   #etag         = filemd5("/Users/yashwant/Downloads/build_3/${each.key}")
+#   #content_type  = "text/html"
+# }
+
+module "test_aws_s3_folder_1" {
+  source = "github.com/chandan-singh/terraform-aws-s3-object-folder.git"
+
+  bucket                = aws_s3_bucket.web-1234.id
+  base_folder_path      = "/Users/yashwant/Downloads/build_3" # Or, something like "~/abc/xyz/build"
+  file_glob_pattern     = "**"
+  set_auto_content_type = true
 }
 
 #### for obtaining the endpoint####
